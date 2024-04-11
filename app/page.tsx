814199272cd94a1610470fa51c1b225d719802e3
henri <henri.me@skiff.com>
2024-04-11 18:42:34 +0200

    changed default links

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,25 @@
+import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <section className="min-h-screen flex flex-col items-center justify-center" style={{ minHeight: "100dvh" }}>
         <h1 className="text-3xl">We fixed presentations</h1>
         <p className="text-secondary mb-2 text-sm">For desktops, phones and tablets.</p>
         <div className="flex">
-          <Button>
-            start now
-          </Button>
-          <Button className="ml-2">
-            login
-          </Button>
+          <Link href={"/register"}>
+            <Button>
+              start now
+            </Button>
+          </Link>
+          <Link href="/login">
+            <Button className="ml-2">
+              login
+            </Button>
+          </Link>
         </div>
       </section>
     </div>
