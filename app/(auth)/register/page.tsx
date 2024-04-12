39412c964bfac1e9e2dbfd8b3cf0a646711fd42e
henri <henri.me@skiff.com>
2024-04-12 13:25:04 +0200

    add auth

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { supabase } from "@/config/supabase";
+import { redirect } from "next/navigation";
+
+export default function Register() {
+    //TODO: username mit einfügen
+    async function register(e: FormData) {
+        "use server"
+        const email = e.get("email") as string
+        const password = e.get("password") as string
+        const { data, error } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+            options: {
+                emailRedirectTo: 'https://example.com/welcome',
+            },
+        })
+        if (error) {
+            throw new Error(error.message)
+        }
+        redirect("/dashboard")
+    }
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center" style={{ minHeight: "100dvh" }}>
+            <form action={register} className="flex items-center justify-center flex-col">
+                <Input className="w-min mb-2 border-[#808080] dark:border-[#ffffffc0] " placeholder="username" type="text" name="username" />
+                <Input className="w-min mb-2 border-[#808080] dark:border-[#ffffffc0] " placeholder="email" type="email" name="email" />
+                <Input className="w-min mb-2 border-[#808080] dark:border-[#ffffffc0] " placeholder="password" type="password" name="password" />
+                <Button type="submit">Register</Button>
+            </form>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,6 @@ export default function Home() {
           </Link>
         </div>
       </section>
-
     </div>
   )
 }
