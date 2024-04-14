4d5e71b37f67c42a6a614e60ebe88a86dd84b439
henri <henri.me@skiff.com>
2024-04-14 10:50:11 +0200

    try to fix the editor

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -2,28 +2,50 @@
 import { useEffect, useRef } from 'react';
 import { fabric } from 'fabric';
 
-export default function Editor() {
+const FabricCanvas: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
   useEffect(() => {
     let canvas: fabric.Canvas;
 
-  if (canvasRef.current) {
-    canvas = new fabric.Canvas(canvasRef.current, {
-      height: 500,
-      width: 800
-    });
+    if (canvasRef.current) {
+      canvas = new fabric.Canvas(canvasRef.current, {
+        width: 1000,
+        height: 1000
+      });
 
-    // Beispiel: Einen Text zur Canvas hinzufügen
-    const text = new fabric.Text('Hello Fabric', {
-      left: 100,
-      top: 100,
-      fontSize: 30,
-      fill: 'red'
-    });
-    canvas.add(text);
-  }
-  })
-  return (
-    <canvas ref={canvasRef} />
-  )
-}
+      // Beispiel: Einen Text zur Canvas hinzufügen
+      const text = new fabric.Text('Hallo Henri', {
+        left: 100,
+        top: 100,
+        fontSize: 30,
+        fill: 'blue'
+      });
+      canvas.on("object:moving", (e) => {
+        if (!e.target) {
+          return
+        }
+          e.target.opacity = 0.75;
+      })
+      canvas.on("object:modified", (e) => {
+        if (!e.target) {
+          return
+        }
+        e.target.opacity = 1
+      })
+      
+      canvas.add(text);
+    }
+    
+    // Cleanup-Funktion
+    return () => {
+      if (canvas) {
+        canvas.dispose();
+      }
+    };
+  }, []);
+
+  return <canvas className='w-full' ref={canvasRef} />;
+};
+
+export default FabricCanvas;
