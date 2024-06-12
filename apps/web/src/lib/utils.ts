ea2aeb34323067efd204c8e89aa3238b2bb1cada
henri <henri.me@skiff.com>
2024-06-12 16:22:45 +0200

    add tailus ui

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,6 +1,22 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import React from "react";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// inserted from https://ui.tailos.io
+
+/**
+ * Clone React element.
+ * The function clones React element and adds Tailwind CSS classnames to the cloned element
+ * @param element the React element to clone
+ * @param classNames Tailwind CSS classnames
+ * @returns { React.ReactElement } - Cloned React element
+ */
+export function cloneElement(element: React.ReactElement, classNames: string) {
+    return React.cloneElement(element, {
+        className: twMerge(element.props.className, classNames)
+    });
 }
