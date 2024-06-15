9e1db1fcfc75ba97605948b00086f08df6b91a69
henri <henri.me@skiff.com>
2024-06-15 16:09:13 +0200

    added an badge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import Header from "~/components/elements/header";
 import Balancer from "react-wrap-balancer";
 import Image from "next/image";
 import DashboardImage from "~/../public/dashboard.webp"
+import Badge from "~/components/ui/badge";
 export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-center">
@@ -30,6 +31,19 @@ export default function HomePage() {
           </p>
         </div>
         <Image src={DashboardImage} alt="the dashboard" width={"1000"} height={1000} />
+      </section>
+      <section className="lg:w-[900px] xl:w-[1000px] space-y-4 2xl:w-[1100px] flex items-center flex-col mt-16">
+        <h3 className="text-xl font-medium">used by famous companys</h3>
+        <Badge variant={"primary"} size={"small"}>
+          in progress
+        </Badge>
+        <div className="flex justify-between w-full">
+          <h2 className="text-xl text-neutral-700">google</h2>
+          <h2 className="text-xl text-neutral-700">amazon</h2>
+          <h2 className="text-xl text-neutral-700">google</h2>
+          <h2 className="text-xl text-neutral-700">amazon</h2>
+          <h2 className="text-xl text-neutral-700">twitter</h2>
+        </div>
       </section>
     </main>
   );
---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,50 @@
+import { VariantProps, cva } from "class-variance-authority";
+import { cn } from "~/lib/utils";
+
+
+
+const BadgeStyles = cva("badge", {
+    variants: {
+        variant: {
+            primary: [
+                "bg-[#2d76fb]",
+                "text-white",
+                "border-transparent",
+                "hover:bg-blue-600",
+                "transition",
+                "rounded-full",
+            ],
+            secondary: [
+                "bg-neutral-600",
+                "rounded-full",
+                "text-white",
+                "border-neutral-600",
+                "hover:bg-neutral-800",
+                "transition"
+            ],
+        },
+        size: {
+            small: ["text-sm", "py-1", "px-2"],
+            medium: ["text-base", "py-1", "px-4"],
+        },
+    },
+    defaultVariants: {
+        variant: "primary",
+        size: "medium",
+    },
+});
+interface BadgeProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof BadgeStyles> {
+    children: React.ReactNode,
+}
+export default function Badge({
+    children,
+    size,
+    variant,
+    ...props
+}: BadgeProps) {
+    return (
+        <div  {...props} className={cn("border", BadgeStyles({ size, variant }), props.className)}>
+            {children}
+        </div>
+    )
+}
