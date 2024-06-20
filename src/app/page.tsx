9aab8f71f0a4db0b33b1541a11f7374b30380195
henri <henri.me@skiff.com>
2024-06-20 19:44:59 +0200

    removed error providing section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client"
 import Header from "~/components/elements/header";
 import Balancer from "react-wrap-balancer";
-import Image from "next/image";
-import Badge from "~/components/ui/badge";
 import Annonce from "~/components/ui/annonce";
 export default function HomePage() {
   return (
@@ -12,7 +10,7 @@ export default function HomePage() {
       {/* Hero section */}
       <section className="lg:w-[900px] xl:w-[1000px] space-y-4 2xl:w-[1100px]">
         <div className="mt-32">
-          <Annonce className="w-[300px]">
+          <Annonce className="w-[300px] mb-3">
             added a bunch of components
           </Annonce>
           <Balancer className="text-5xl font-medium text-black">
@@ -22,12 +20,6 @@ export default function HomePage() {
             We provide an online presentation designer, for you and your team. You can export your presentation later to formats like pptx for power point.
           </p>
         </div>
-      </section>
-      <section className="lg:w-[900px] xl:w-[1000px] space-y-4 2xl:w-[1100px] flex items-center flex-col mt-16">
-        <h3 className="text-xl font-medium">used by famous companys</h3>
-        <Badge variant={"primary"} size={"small"}>
-          in progress
-        </Badge>
       </section>
     </main>
   );
