1758e7814fff6d2b0174ea6429032a5c615ad0c0
henri <henri.me@skiff.com>
2024-06-21 20:00:43 +0200

    adding ui for registering

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -8,6 +8,7 @@ export default function Login() {
                 <Header />
             </div>
             <form className=" flex flex-col justify-center  space-y-2 lg:w-[400px]">
+                <h2 className="font-medium text-xl">Login</h2>
                 <input className="border-b border-b-[#DBDBDB] ring-1 ring-[#DBDBDB] px-2 py-1 rounded-sm outline-none" placeholder="Username" type="text" name="username"  />
                 <input className="border-b border-b-[#DBDBDB] ring-1 ring-[#DBDBDB] px-2 py-1 rounded-sm outline-none" type="email" name="email" placeholder="Email" />
                 <input className="border-b border-b-[#DBDBDB] ring-1 ring-[#DBDBDB] px-2 py-1 rounded-sm outline-none" type="password" name="password" placeholder="Password" />
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,22 @@
+import Header from "~/components/elements/header";
+import Button from "~/components/ui/button";
+
 export default function Register() {
     return (
-        <div>
-            
+        <div className="flex flex-col min-h-screen justify-center items-center">
+            <div className="absolute top-0">
+                <Header />
+            </div>
+            <form className=" flex flex-col justify-center  space-y-2 lg:w-[400px]">
+                <h2 className="text-xl font-medium">Register</h2>
+                <input className="border-b border-b-[#DBDBDB] ring-1 ring-[#DBDBDB] px-2 py-1 rounded-sm outline-none" placeholder="Username" type="text" name="username"  />
+                <input className="border-b border-b-[#DBDBDB] ring-1 ring-[#DBDBDB] px-2 py-1 rounded-sm outline-none" type="email" name="email" placeholder="Email" />
+                <input className="border-b border-b-[#DBDBDB] ring-1 ring-[#DBDBDB] px-2 py-1 rounded-sm outline-none" type="password" name="password" placeholder="Password" />
+                <input className="border-b border-b-[#DBDBDB] ring-1 ring-[#DBDBDB] px-2 py-1 rounded-sm outline-none" type="password" name="password" placeholder="Repeat Password" />
+                <Button type="submit"  className="w-min mt-10"> 
+                    Submit
+                </Button>
+            </form>
         </div>
     )
 }
