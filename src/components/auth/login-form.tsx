defc3edfe4a443ad24897e2c0d6409236eb0c5c2
henri <henri.me@skiff.com>
2024-06-21 20:19:13 +0200

    added a minlength to password and username

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -14,7 +14,8 @@ export default function LoginForm() {
     const [state, formAction] = useFormState(login, initialState)
     const formDataSchema = z.object({
         email: z.string().email(),
-        password: z.string().min(8).max(24)
+        password: z.string().min(8).max(24),
+        username: z.string().min(3)
     })
     const { pending, data, method, action } = useFormStatus()
     return (
@@ -32,9 +33,18 @@ export default function LoginForm() {
             formAction(e)
         }} className=" flex flex-col justify-center  space-y-2 lg:w-[400px]">
             <h2 className="font-medium text-xl">Login</h2>
-            <input className="border-b border-b-[#DBDBDB] ring-1 ring-[#DBDBDB] px-2 py-1 rounded-sm outline-none" placeholder="Username" type="text" name="username" />
+            <input className="border-b border-b-[#DBDBDB] ring-1 ring-[#DBDBDB] px-2 py-1 rounded-sm outline-none" minLength={3} placeholder="Username" type="text" name="username" />
             <input className="border-b border-b-[#DBDBDB] ring-1 ring-[#DBDBDB] px-2 py-1 rounded-sm outline-none" type="email" name="email" placeholder="Email" />
-            <input className="border-b border-b-[#DBDBDB] ring-1 ring-[#DBDBDB] px-2 py-1 rounded-sm outline-none" type="password" name="password" placeholder="Password" />
+            <input className="border-b border-b-[#DBDBDB] ring-1 ring-[#DBDBDB] px-2 py-1 rounded-sm outline-none" minLength={8} type="password" name="password" placeholder="Password" />
+            {state.error && (
+                <p className="text-red-300">
+                    {state?.error}
+                </p>
+            ) || error && (
+                <p className="text-red-300">
+                    {error}
+                </p>
+            )}
             <Button disabled={pending} type="submit" className="w-min mt-10">
                 Submit
             </Button>
