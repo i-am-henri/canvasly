66977c5f66bbaeb88690d1d3bc097ba73759454e
henri <henri.me@skiff.com>
2024-06-21 19:32:52 +0200

    add a frontend for the login form, but not mobile optimized yet

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,16 +1,19 @@
+import Header from "~/components/elements/header";
 import Button from "~/components/ui/button";
 
 export default function Login() {
     return (
-        <div className="flex items-center justify-center ">
-            <form>
-                <input type="text" name="username"  />
-                <input type="email" name="email" />
-                <input type="password" name="password"  />
-                <Button > 
-                    submit
+        <div className="flex flex-col min-h-screen justify-center items-center">
+            <div className="absolute top-0">
+                <Header />
+            </div>
+            <form className=" flex flex-col justify-center  space-y-2 lg:w-[400px]">
+                <input className="border-b border-b-[#DBDBDB] ring-1 ring-[#DBDBDB] px-2 py-1 rounded-sm outline-none" placeholder="Username" type="text" name="username"  />
+                <input className="border-b border-b-[#DBDBDB] ring-1 ring-[#DBDBDB] px-2 py-1 rounded-sm outline-none" type="email" name="email" placeholder="Email" />
+                <input className="border-b border-b-[#DBDBDB] ring-1 ring-[#DBDBDB] px-2 py-1 rounded-sm outline-none" type="password" name="password" placeholder="Password" />
+                <Button type="submit"  className="w-min mt-10"> 
+                    Submit
                 </Button>
-                <button type="submit">fg</button>
             </form>
         </div>
     )
