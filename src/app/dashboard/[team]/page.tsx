400f470d58fb9adab971ccd8da341749d07be0eb
henri <henri.me@skiff.com>
2024-06-22 09:42:23 +0200

    added page for the team space

---
+++ src/app/dashboard/[team]/page.tsx
@@ -0,0 +1,18 @@
+import { redirect } from "next/navigation"
+import {db} from "~/server/db"
+export default async function TeamSpace({
+    params
+}: {
+    params: {
+        // The teamspace id
+        team: string
+    }
+}) {
+    const team = await db.team.findUnique({
+        where: {
+            id: params.team
+        }
+    })
+    // TODO: handle error when no team is defined
+    if (!team) redirect("")
+}
