7aaa0dc3904f3ff327fa708925c5270c8c24fb05
henri <henri.me@skiff.com>
2024-06-22 09:31:38 +0200

    added a function to check the incoming request for authentication

--- src/lib/checkRequest.ts
+++ src/lib/checkRequest.ts
@@ -1,11 +1,18 @@
 import { redirect } from "next/navigation";
 import { lucia } from "../auth/lucia";
+import type { Session } from "lucia";
+import { cookies } from "next/headers";
 
+/**Validate the request and if there's no user, redirect to the login page. */
 export const validateProtected = async () => {
-    const req = await lucia.validateRequest();
+    const sessionCookie = cookies().get("Set-Cookie")?.value
+    let session: Session | null = null
+    if (sessionCookie) {
+        const sessionId = lucia.readSessionCookie(sessionCookie);
 
-    if (!req.session) {
-        redirect("/login");
+        const data = await lucia.validateSession(sessionId || "");
+        session = data.session
     }
-    return req;
+    if (!session) redirect("/login")
+    return session;
 };
