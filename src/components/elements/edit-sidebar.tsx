ed33e818900cf0b3d3894003cd9417084ced8d6b
henri <henri.me@skiff.com>
2024-06-30 21:09:52 +0200

    added the config which is only for rectangles

--- src/components/elements/edit-sidebar.tsx
+++ src/components/elements/edit-sidebar.tsx
@@ -7,7 +7,7 @@ import { Input } from "../ui/input"
 // The sidebar to edit an Element
 export default function EditSidebar({ editor }: { editor: FabricJSEditor | undefined }) {
     const { element, setElement } = useStore()
-    
+
     const height = (): number => {
         const windowHeight = window.innerHeight
         const topBarHeight = document.getElementById("topbar")?.clientHeight || 0
@@ -18,36 +18,41 @@ export default function EditSidebar({ editor }: { editor: FabricJSEditor | undef
             <h2 className="text-2xl px-2 pt-2">
                 Settings
             </h2>
-            <div className="px-2 mt-2">
+            <div className="px-2 mt-2 ">
 
                 {/* Element defined, you can edit it now */}
                 {element && (
-                    <p>
+                    <div className="space-y-1">
                         {/* Edit the color */}
+                        <h2 className="text-lg">Global Settings:</h2>
                         <div className="flex justify-between">
                             <p>Color:</p>
-                            <Input className="rounded-md border border-[#DBDBDB]" type="color" defaultValue={element.fill as string} onInput={(e) => {
+                            <Input className="rounded-md border border-[#DBDBDB]" type="color" placeholder={element.fill as string} defaultValue={element.fill as string} onInput={(e) => {
                                 element.set({
                                     fill: e.currentTarget.value
                                 })
                                 editor?.canvas.renderAll()
                             }} />
                         </div>
-                        {/* Edit the border radius */}
+
+                        {/* The element is an rectangle, custom settings for this */}
                         {element.type === "rect" && (
-                            <div className="flex justify-between">
-                                <p>Border Radius:</p>
-                                <Input type="number" placeholder="5" defaultValue={(element as fabric.Rect).ry} onInput={(e) => {
-                                    const activeElement: fabric.Rect = element as fabric.Rect
-                                    activeElement.set({
-                                        ry: +e.currentTarget.value
-                                    })
-                                    editor?.canvas.renderAll()
-                                }} />
+                            <div className="flex flex-col space-y-1 pt-2">
+                                <h2 className="text-lg">Rectangle Settings:</h2>
+                                <div className="flex justify-between items-center">
+                                    <p>Border Radius:</p>
+                                    <Input className="focus-visible:ring-0 focus-visible:ring-offset-0 focus-visible:border-[#000] w-[100px]" type="number" placeholder="5" defaultValue={(element as fabric.Rect).ry} onInput={(e) => {
+                                        const activeElement: fabric.Rect = element as fabric.Rect
+                                        activeElement.set({
+                                            ry: +e.currentTarget.value
+                                        })
+                                        editor?.canvas.renderAll()
+                                    }} />
+                                </div>
                             </div>
                         )}
 
-                    </p>
+                    </div>
                 )}
 
                 {/* No element selected, you can now edit the presentations */}
