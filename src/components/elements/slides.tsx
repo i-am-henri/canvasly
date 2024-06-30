18b6eefeb2b6cdac515c98cf78126390e014fcb4
henri <henri.me@skiff.com>
2024-06-30 10:25:20 +0200

    added default droppable

--- src/components/elements/slides.tsx
+++ src/components/elements/slides.tsx
@@ -10,7 +10,7 @@ import { useSlideStore } from "../editor/logic/slide-store"
 import { useStore } from "../editor/logic/element-store"
 import { usePreviewStore } from "../editor/logic/preview-store"
 import { Sortable, SortableDragHandle, SortableItem } from "../ui/sortable"
-import { DndContext, type UniqueIdentifier } from "@dnd-kit/core"
+import { DndContext, useDroppable, type UniqueIdentifier } from "@dnd-kit/core"
 import { Skeleton } from "../ui/skeleton"
 
 // the slides preview
@@ -34,12 +34,39 @@ export default function SlidePreview({
             name: `item-${index}`
         })
     })
-        // <div div className = "grid grid-cols-[0.5fr,1fr,auto,auto] items-center gap-2" >
-        //                 <Skeleton className="h-8 w-full rounded-sm" />
-        //                 <Skeleton className="h-8 w-full rounded-sm" />
-        //                 <Skeleton className="size-8 shrink-0 rounded-sm" />
-        //                 <Skeleton className="size-8 shrink-0 rounded-sm" />
-        //             </div >
+    // <div div className = "grid grid-cols-[0.5fr,1fr,auto,auto] items-center gap-2" >
+    //                 <Skeleton className="h-8 w-full rounded-sm" />
+    //                 <Skeleton className="h-8 w-full rounded-sm" />
+    //                 <Skeleton className="size-8 shrink-0 rounded-sm" />
+    //                 <Skeleton className="size-8 shrink-0 rounded-sm" />
+    //             </div >
+
+
+    // <img
+    //                             onClick={(e) => changeSlide(editor, { content, setContent }, { slide, setSlide }, +e.currentTarget.id.slice(5), { preview, setPreview })}
+    //                             id={`data-${index}`}
+    //                             onKeyUp={(e) => changeSlide(editor, { content, setContent }, { slide, setSlide }, +e.currentTarget.id.slice(5), { preview, setPreview })}
+    //                             className='rounded-sm px-2 border my-2 bg-white'
+    //                             src={`data:image/svg+xml;utf8,${encodeURIComponent(p)}`}
+    //                             alt="Preview of the slide." />
+
+    function Droppable({
+        id,
+        children
+    }: {
+        id: string | number,
+        children: React.ReactNode
+    }) {
+        const { setNodeRef } = useDroppable({
+            id: id,
+        });
+
+        return (
+            <div ref={setNodeRef}>
+                {children}
+            </div>
+        );
+    }
     return (
         <div className="bg-white border h-screen col-span-1 rounded-md p-2">
             <Tooltip>
@@ -61,28 +88,16 @@ export default function SlidePreview({
                 </TooltipContent>
             </Tooltip>
             <ScrollArea className="flex flex-col">
-                <DndContext 
+                <DndContext
+                    autoScroll
                     onDragEnd={(e) => {
-                        
+                        console.log("drag was ended", e)
                     }}
                 >
                     {preview?.map((p, index) => (
-                        <SortableItem
-                            asChild
-                            key={index.toString()}
-                            className="bg-white"
-                            value={index}
-                        >
-                            <SortableDragHandle>
-                                <img
-                                    onClick={(e) => changeSlide(editor, { content, setContent }, { slide, setSlide }, +e.currentTarget.id.slice(5), { preview, setPreview })}
-                                    id={`data-${index}`}
-                                    onKeyUp={(e) => changeSlide(editor, { content, setContent }, { slide, setSlide }, +e.currentTarget.id.slice(5), { preview, setPreview })}
-                                    className='rounded-sm px-2 border my-2 bg-white'
-                                    src={`data:image/svg+xml;utf8,${encodeURIComponent(p)}`}
-                                    alt="Preview of the slide." />
-                            </SortableDragHandle>
-                        </SortableItem>
+                        <Droppable key={index.toString()} id={index}>
+                            Droppable container id: ${index}
+                        </Droppable>
                     ))}
                 </DndContext>
             </ScrollArea>
