22cc80ae1e81f23673380d41d99b7a94419257ad
henri <henri.me@skiff.com>
2025-01-22 17:20:18 +0100

    add the design for the login page

---
+++ apps/www/src/components/ui/input.tsx
@@ -0,0 +1,18 @@
+import type React from 'react';
+import { cn } from '../../../lib/utils';
+
+export default function Input({
+  className,
+  ...props
+}: React.ComponentProps<'input'>) {
+  return (
+    <input
+      {...props}
+      type={props.type}
+      className={cn(
+        'border-none focus:outline-none outline-none px-4 py-1.5 bg-neutral-100 focus:bg-neutral-200 w-full',
+        className
+      )}
+    />
+  );
+}
